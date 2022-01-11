e33b6138ceec2b5b14ca4be80ec08aef9bd58328
pgg-dev <park.geumgyu@gmail.com>
2022-01-11 19:33:02 +0900

    커스텀 훅 생성 및 적용

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,21 +1,14 @@
 import React, { useCallback, useState } from "react";
 import { Form, Label, Input, LinkContainer, Button, Header, Error } from "./styles";
 import { Link } from "react-router-dom";
+import useInput from "@hooks/useInput";
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [email, onChangeEmail] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback(
     (e) => {
