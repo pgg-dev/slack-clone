207de502bc99c5847854fc54e71bd0b60180b93e
pgg-dev <park.geumgyu@gmail.com>
2022-01-25 05:56:15 +0900

    swr 설정 추가

--- front/pages/LogIn/index.tsx
+++ front/pages/LogIn/index.tsx
@@ -7,7 +7,9 @@ import { Link } from "react-router-dom";
 import useSWR from "swr";
 
 const LogIn = () => {
-  const { data, error } = useSWR("http://localhost:3095/api/users", fetcher);
+  const { data, error, mutate } = useSWR("http://localhost:3095/api/users", fetcher, {
+    dedupingInterval: 100000,
+  });
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput("");
   const [password, onChangePassword] = useInput("");
@@ -17,7 +19,9 @@ const LogIn = () => {
       setLogInError(false);
       axios
         .post("http://localhost:3095/api/users/login", { email, password }, { withCredentials: true })
-        .then(() => {})
+        .then(() => {
+          mutate();
+        })
         .catch((error) => {
           setLogInError(error.response?.data?.statusCode === 401);
         });
